d3c5d557655753477dd7caa26f61d75b60945901
bar0191 <brandonreid2@my.unt.edu>
2018-09-09 21:10:10 -0500

    updated nextjs routes

--- routes.js
+++ routes.js
@@ -2,3 +2,6 @@ const nextRoutes = require('next-routes');
 const routes = module.exports = nextRoutes();
 
 routes.add('index', '/');
+routes.add('works', '/works');
+routes.add('about', '/about');
+routes.add('tac', '/works/tac');
